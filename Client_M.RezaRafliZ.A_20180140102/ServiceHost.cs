8500d946a34029214566133fd3a60d99fd3869c2
Mochammad Reza Rafli Zamzam Alfarizi <rezarafli9668@gmail.com>
2020-10-31 14:58:51 +0700

    Add project files.

---
+++ Client_M.RezaRafliZ.A_20180140102/ServiceHost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Client_M.RezaRafliZ.A_20180140102
+{
+    internal class ServiceHost
+    {
+        private Type type;
+
+        public ServiceHost(Type type)
+        {
+            this.type = type;
+        }
+
+        internal void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Close()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server_M.RezaRafliZ.A_20180140102/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140102;
+using System.Windows.Forms;
+
+namespace Server_M.RezaRafliZ.A_20180140102
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
